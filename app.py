b5a60e553f033f3fb60b430272aed37be34070ae
Robert Roskam <raiderrobert@gmail.com>
2019-02-05 16:28:10 -0500

    all the things

---
+++ app.py
@@ -0,0 +1,29 @@
+from starlette.applications import Starlette
+from starlette.staticfiles import StaticFiles
+from starlette.responses import HTMLResponse, JSONResponse
+import uvicorn
+
+
+app = Starlette(debug=True, template_directory='templates')
+app.mount('/static', StaticFiles(directory='statics'), name='static')
+
+VOTES = []
+
+@app.route('/')
+async def homepage(request):
+    template = app.get_template('index.html')
+    content = template.render(request=request)
+    return HTMLResponse(content)
+
+@app.route('/vote_total', methods=['GET'])
+async def vote_total(request):
+    return JSONResponse({'total': sum(VOTES)})
+
+@app.route('/cast_vote', methods=['POST'])
+async def cast_vote(request):
+    VOTES.append(1)
+    return JSONResponse({'status': 'ok'})
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
+
